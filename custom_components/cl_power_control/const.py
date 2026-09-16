108d2fb939c24e13cc7f0eff436f409df65f0316
climpianti <161598495+climpianti@users.noreply.github.com>
2026-09-16 18:47:13 +0200

    Bump development version to 0.4.0-dev.9

--- custom_components/cl_power_control/const.py
+++ custom_components/cl_power_control/const.py
@@ -5,7 +5,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "cl_power_control"
 NAME = "CL Power Control"
-VERSION = "0.4.0-dev.8"
+VERSION = "0.4.0-dev.9"
 MANUFACTURER = "CL Impianti"
 
 CONF_NAME = "name"
